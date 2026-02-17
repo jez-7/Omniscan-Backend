f56aa68474a12103c1be32c995b13cbbc4211e56
jez-7 <juanemanuelzamora@gmail.com>
2026-02-16 23:14:00 -0300

    pls work

--- src/test/java/com/monitoreo/MonitoreoApplicationTests.java
+++ src/test/java/com/monitoreo/MonitoreoApplicationTests.java
@@ -1,23 +1,16 @@
 package com.monitoreo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration;
-import org.springframework.boot.data.redis.autoconfigure.DataRedisReactiveAutoConfiguration;
-import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-
-import org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAutoConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest
 @EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
-@ImportAutoConfiguration(exclude = {
-        MongoAutoConfiguration.class,
-        DataMongoAutoConfiguration.class,
-        DataRedisReactiveAutoConfiguration.class,
-        DataRedisRepositoriesAutoConfiguration.class
+@TestPropertySource(properties = {
+        "spring.data.mongodb.auto-index-creation=false",
+        "spring.data.redis.repositories.enabled=false"
 })
 class MonitoreoApplicationTests {
 
