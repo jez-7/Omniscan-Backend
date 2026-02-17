5d5a0476d17ea4e34c78559b8ace0092331fa476
jez-7 <juanemanuelzamora@gmail.com>
2026-02-17 00:18:01 -0300

    this wont work x2

--- src/test/java/com/monitoreo/MonitoreoApplicationTests.java
+++ src/test/java/com/monitoreo/MonitoreoApplicationTests.java
@@ -3,17 +3,16 @@ package com.monitoreo;
 import com.monitoreo.repository.PriceRepository;
 import com.monitoreo.service.PriceConsumer;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration;
 import org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration;
+import org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration;
 import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -32,14 +31,34 @@ import static org.mockito.Mockito.mock;
         MongoAutoConfiguration.class,
         DataMongoAutoConfiguration.class,
         DataRedisAutoConfiguration.class,
-        DataRedisAutoConfiguration.class
+        DataRedisAutoConfiguration.class,
+        KafkaAutoConfiguration.class
 })
 @TestPropertySource(properties = {
         "spring.main.lazy-initialization=true",
         "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}",
-        "spring.kafka.admin.auto-create=false"
+        "spring.kafka.admin.auto-create=false",
+        "spring.data.mongodb.port=0",
+        "spring.data.redis.port=0"
 })
+@Import(MonitoreoApplicationTests.Config.class)
 class MonitoreoApplicationTests {
+
+    @TestConfiguration
+    static class Config {
+        @Bean
+        public PriceRepository priceRepository() {
+            return Mockito.mock(PriceRepository.class);
+        }
+        @Bean
+        public MongoTemplate mongoTemplate() {
+            return Mockito.mock(MongoTemplate.class);
+        }
+        @Bean
+        public StringRedisTemplate stringRedisTemplate() {
+            return Mockito.mock(StringRedisTemplate.class);
+        }
+    }
 
     @Test
     void contextLoads() {
