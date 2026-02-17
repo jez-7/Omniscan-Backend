8c4d7a5cfeffc487bd27f4bfbcff533b4c149739
jez-7 <juanemanuelzamora@gmail.com>
2026-02-16 23:51:33 -0300

    .

--- src/test/java/com/monitoreo/MonitoreoApplicationTests.java
+++ src/test/java/com/monitoreo/MonitoreoApplicationTests.java
@@ -12,39 +12,8 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import static org.mockito.Mockito.mock;
 
 
-@SpringBootTest(
-        properties = {
-                "spring.autoconfigure.exclude=" +
-                        "org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration," +
-                        "org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration"
-        }
-)
+@SpringBootTest()
 class MonitoreoApplicationTests {
-
-    @TestConfiguration
-    static class TestConfig {
-
-        @Bean
-        @Primary
-        public MongoTemplate mongoTemplate() {
-            return mock(MongoTemplate.class);
-        }
-
-        @Bean
-        @Primary
-        public RedisTemplate<String, Object> redisTemplate() {
-            return mock(RedisTemplate.class);
-        }
-
-        @Bean
-        @Primary
-        public KafkaTemplate<String, Object> kafkaTemplate() {
-            return mock(KafkaTemplate.class);
-        }
-    }
 
     @Test
     void contextLoads() {
