1b1d072b8211e1df354fe0ba4b20472373258cf7
jez-7 <juanemanuelzamora@gmail.com>
2026-02-16 23:15:39 -0300

    pls work x2

--- src/test/java/com/monitoreo/MonitoreoApplicationTests.java
+++ src/test/java/com/monitoreo/MonitoreoApplicationTests.java
@@ -7,11 +7,6 @@ import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest
-@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
-@TestPropertySource(properties = {
-        "spring.data.mongodb.auto-index-creation=false",
-        "spring.data.redis.repositories.enabled=false"
-})
 class MonitoreoApplicationTests {
 
 	@Test
