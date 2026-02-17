a36a44b59988b909106730de114c0adbdf86512b
jez-7 <juanemanuelzamora@gmail.com>
2026-02-16 23:31:46 -0300

    adding kafka autoconfig please work

--- src/test/java/com/monitoreo/MonitoreoApplicationTests.java
+++ src/test/java/com/monitoreo/MonitoreoApplicationTests.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration;
 
 import org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration;
+import org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration;
 import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 
 import org.springframework.boot.data.redis.autoconfigure.DataRedisReactiveAutoConfiguration;
 import org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAutoConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 
 @SpringBootTest
@@ -18,8 +20,17 @@ import org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAu
 @ImportAutoConfiguration(exclude = {
         MongoAutoConfiguration.class,
         DataMongoAutoConfiguration.class,
+
         DataRedisAutoConfiguration.class,
-        DataRedisRepositoriesAutoConfiguration.class
+        DataRedisRepositoriesAutoConfiguration.class,
+
+        KafkaAutoConfiguration.class
+})
+@TestPropertySource(properties = {
+        "spring.main.lazy-initialization=true",
+        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.data.mongodb.port=0",
+        "spring.data.redis.port=0"
 })
 class MonitoreoApplicationTests {
 
