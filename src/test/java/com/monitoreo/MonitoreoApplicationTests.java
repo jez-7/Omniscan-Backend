63505af3bfd04c57e2230e7982dfa95f61a3c777
jez-7 <juanemanuelzamora@gmail.com>
2026-02-16 23:12:27 -0300

    new spring 4 autoconfiguration

--- src/test/java/com/monitoreo/MonitoreoApplicationTests.java
+++ src/test/java/com/monitoreo/MonitoreoApplicationTests.java
@@ -1,20 +1,22 @@
 package com.monitoreo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration;
+import org.springframework.boot.data.redis.autoconfigure.DataRedisReactiveAutoConfiguration;
+import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration;
-import org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration;
+
 import org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAutoConfiguration;
 
 
 @SpringBootTest
 @EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @ImportAutoConfiguration(exclude = {
+        MongoAutoConfiguration.class,
         DataMongoAutoConfiguration.class,
-        DataRedisAutoConfiguration.class,
+        DataRedisReactiveAutoConfiguration.class,
         DataRedisRepositoriesAutoConfiguration.class
 })
 class MonitoreoApplicationTests {
