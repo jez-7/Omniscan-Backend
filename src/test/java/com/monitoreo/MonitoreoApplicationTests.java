d03ae6ffab191ba7c007b03b3b8d65f9cab6ab33
jez-7 <juanemanuelzamora@gmail.com>
2026-02-16 23:44:28 -0300

    testing

--- src/test/java/com/monitoreo/MonitoreoApplicationTests.java
+++ src/test/java/com/monitoreo/MonitoreoApplicationTests.java
@@ -1,37 +1,41 @@
 package com.monitoreo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration;
-
-import org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration;
-
-import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-
-import org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAutoConfiguration;
-import org.springframework.test.context.TestPropertySource;
+import static org.mockito.Mockito.mock;
 
 
 @SpringBootTest
-@EmbeddedKafka(partitions = 1)
-@ImportAutoConfiguration(exclude = {
-        org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration.class,
-        org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration.class,
-        org.springframework.boot.data.redis.autoconfigure.DataRedisAutoConfiguration.class,
-        org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAutoConfiguration.class
-})
-@TestPropertySource(properties = {
-        "spring.main.lazy-initialization=true",
-        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}",
-        "spring.data.mongodb.port=0",
-        "spring.data.redis.port=0"
+@EmbeddedKafka(partitions = 1, brokerProperties = {
+        "listeners=PLAINTEXT://localhost:9092",
+        "port=9092"
 })
 class MonitoreoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        @Primary
+        public MongoTemplate mongoTemplate() {
+            return mock(MongoTemplate.class);
+        }
+
+        @Bean
+        @Primary
+        public RedisTemplate<String, Object> redisTemplate() {
+            return mock(RedisTemplate.class);
+        }
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
